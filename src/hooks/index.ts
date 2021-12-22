6762c21268aa147d3af7fa1c7d55b4b5d0b5789f
Dave Quah <quahdave@gmail.com>
2021-12-22 13:45:57 +0800

    feat: add audio hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./useAppDispatch";
 export * from "./useAppSelector";
 export * from "./useDetectClickOutside";
+export * from "./useAudio";
---
+++ src/hooks/useAudio.ts
@@ -0,0 +1,42 @@
+import { useEffect, useMemo, useRef } from "react";
+
+type options = {
+  volumeLevel: number;
+  isPlaying: boolean;
+  trackSrc: string;
+  onTrackEnded: () => void;
+};
+
+export const useAudio = ({
+  volumeLevel,
+  isPlaying,
+  trackSrc,
+  onTrackEnded,
+}: options) => {
+  const audioElement = useMemo(() => new Audio(), []);
+  const audioRef = useRef<HTMLAudioElement>(audioElement);
+
+  // when track changes, update source
+  useEffect(() => {
+    audioRef.current.src = trackSrc;
+  }, [trackSrc]);
+
+  // when track changes or 'play' state changes, play/pause accordingly
+  useEffect(() => {
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying, trackSrc]);
+
+  // when volume changes, update volume
+  useEffect(() => {
+    audioRef.current.volume = volumeLevel / 100;
+  }, [volumeLevel]);
+
+  // when track ends, call onTrackEnded
+  useEffect(() => {
+    audioElement.addEventListener("ended", onTrackEnded);
+
+    return () => {
+      audioElement.removeEventListener("ended", onTrackEnded);
+    };
+  }, [audioElement, onTrackEnded]);
+};
