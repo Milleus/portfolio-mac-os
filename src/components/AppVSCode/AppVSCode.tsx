1914e36e12b4f6338c43c18ede07d7192ba4e34d
Dave Quah <quahdave@gmail.com>
2021-12-30 21:33:27 +0800

    refactor: shift window controls to app component

--- src/components/AppVSCode/AppVSCode.tsx
+++ src/components/AppVSCode/AppVSCode.tsx
@@ -1,17 +1,93 @@
+import { BsX } from "react-icons/bs";
+import { CgArrowsExpandLeft, CgMathMinus } from "react-icons/cg";
 import { FC } from "react";
 
-import Window from "components/Window";
-import { ApplicationKeys } from "reducers/applicationSlice";
+import Window, { dragHandleClass } from "components/Window";
+import { ApplicationKeys, updateApplication } from "reducers/applicationSlice";
+import { useAppDispatch, useAppSelector } from "hooks";
 
 const AppVSCode: FC<Record<string, never>> = () => {
+  const { vscode } = useAppSelector((state) => state.application);
+  const dispatch = useAppDispatch();
+  const isMaximized = vscode.windowStatus === "maximized";
+
+  const handleBarDoubleClick = () => {
+    dispatch(
+      updateApplication({
+        appKey: ApplicationKeys.VSCODE,
+        status: { windowStatus: "maximized" },
+      })
+    );
+  };
+
+  const handleCloseClick = () => {
+    dispatch(
+      updateApplication({
+        appKey: ApplicationKeys.VSCODE,
+        status: { isOpen: false, windowStatus: "normal" },
+      })
+    );
+  };
+
+  const handleMinimizeClick = () => {
+    // do nothing for now
+  };
+
+  const handleMaximizeClick = () => {
+    const windowStatus = isMaximized ? "normal" : "maximized";
+
+    dispatch(
+      updateApplication({
+        appKey: ApplicationKeys.VSCODE,
+        status: { windowStatus },
+      })
+    );
+  };
+
   return (
-    <Window appKey={ApplicationKeys.VSCODE} title="Visual Studio Code">
-      <iframe
-        className="w-full h-full bg-neutral-800"
-        title="Visual Studio Code"
-        frameBorder={0}
-        src="https://github1s.com/Milleus/portfolio-mac-os/blob/main/README.md"
-      />
+    <Window isMaximized={isMaximized}>
+      <div
+        className={`${dragHandleClass} relative h-6 bg-neutral-700 text-center`}
+        onDoubleClick={handleBarDoubleClick}
+      >
+        <div className="absolute h-6 flex items-center mx-2 space-x-2 group">
+          <button
+            className="w-3 h-3 flex justify-center items-center rounded-full bg-red-500"
+            onClick={handleCloseClick}
+          >
+            <BsX className="invisible group-hover:visible" />
+          </button>
+
+          <button
+            className="w-3 h-3 flex justify-center items-center rounded-full bg-amber-500"
+            onClick={handleMinimizeClick}
+          >
+            <CgMathMinus className="invisible group-hover:visible" />
+          </button>
+
+          <button
+            className="w-3 h-3 flex justify-center items-center rounded-full bg-green-500"
+            onClick={handleMaximizeClick}
+          >
+            <CgArrowsExpandLeft className="invisible group-hover:visible p-0.5" />
+          </button>
+        </div>
+        <span className="font-semibold text-white text-sm">
+          Visual Studio Code
+        </span>
+      </div>
+
+      <div
+        className="w-full overflow-y-hidden"
+        style={{ height: "calc(100% - 1.5rem)" }} // offset h-6, height of drag handle
+      >
+        <iframe
+          className="w-full h-full bg-neutral-800"
+          title="Visual Studio Code"
+          frameBorder={0}
+          src="https://github1s.com/Milleus/portfolio-mac-os/blob/main/README.md"
+        />
+      </div>
     </Window>
   );
 };
--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,14 +1,11 @@
-import { BsX } from "react-icons/bs";
-import { CgArrowsExpandLeft, CgMathMinus } from "react-icons/cg";
 import { FC, ReactNode, useEffect, useState } from "react";
 import { Rnd, RndResizeCallback, RndDragCallback } from "react-rnd";
+import classNames from "classnames";
 
 import { convertRemToPixels } from "utilities";
 import { HEIGHT_DOCK_REM } from "components/Dock";
 import { HEIGHT_MENU_BAR_REM } from "components/MenuBar";
-import { useAppDispatch, useAppSelector, useWindowSize } from "hooks";
-import { ApplicationKeys, updateApplication } from "reducers/applicationSlice";
-import classNames from "classnames";
+import { useWindowSize } from "hooks";
 
 type PositionSize = {
   x: number;
@@ -17,31 +14,26 @@ type PositionSize = {
   height: number;
 };
 
-const dragHandleClass = "drag-handle";
+export const dragHandleClass = "drag-handle";
 
 export type WindowProps = {
-  appKey: ApplicationKeys;
+  isMaximized: boolean;
   children: ReactNode;
-  title?: string;
   defaultWidth?: number;
   defaultHeight?: number;
 };
 
 const Window: FC<WindowProps> = ({
-  appKey,
+  isMaximized,
   children,
-  title,
   defaultWidth,
   defaultHeight,
 }) => {
-  const applicationState = useAppSelector((state) => state.application);
-  const dispatch = useAppDispatch();
   const { winWidth, winHeight } = useWindowSize();
   const initWidth = Math.min(winWidth, defaultWidth ? defaultWidth : 640);
   const initHeight = Math.min(winHeight, defaultHeight ? defaultHeight : 400);
   const heightMenuBarPx = convertRemToPixels(HEIGHT_MENU_BAR_REM);
   const heightDockPx = convertRemToPixels(HEIGHT_DOCK_REM);
-  const isMaximized = applicationState[appKey].windowStatus === "maximized";
 
   const [positionSize, setPositionSize] = useState<PositionSize>({
     width: initWidth,
@@ -78,38 +70,13 @@ const Window: FC<WindowProps> = ({
     });
   };
 
-  const handleBarDoubleClick = () => {
-    dispatch(
-      updateApplication({ appKey, status: { windowStatus: "maximized" } })
-    );
-  };
-
-  const handleCloseClick = () => {
-    dispatch(
-      updateApplication({
-        appKey,
-        status: { isOpen: false, windowStatus: "normal" },
-      })
-    );
-  };
-
-  const handleMinimizeClick = () => {
-    // do nothing for now
-  };
-
-  const handleMaximizeClick = () => {
-    const windowStatus = isMaximized ? "normal" : "maximized";
-
-    dispatch(updateApplication({ appKey, status: { windowStatus } }));
-  };
-
   const width = isMaximized ? winWidth : positionSize.width;
   const height = isMaximized ? winHeight : positionSize.height;
   const x = isMaximized ? winWidth : Math.max(0, positionSize.x); // winWidth because of window boundary
   const y = isMaximized ? -heightMenuBarPx : Math.max(0, positionSize.y); // -heightMenuBarPx because of window boundary
 
   const windowClassNames = classNames({
-    "absolute w-full h-full rounded overflow-hidden shadow-md": true,
+    "absolute w-full h-full rounded-lg overflow-hidden shadow-md": true,
     "z-30": !isMaximized,
     "z-50": isMaximized,
   });
@@ -126,38 +93,7 @@ const Window: FC<WindowProps> = ({
       onDragStop={handleDragStop}
       onResizeStop={handleResizeStop}
     >
-      <div
-        className={`${dragHandleClass} relative h-6 bg-neutral-700 text-center`}
-        onDoubleClick={handleBarDoubleClick}
-      >
-        <div className="absolute h-6 flex items-center mx-2 space-x-2 group">
-          <button
-            className="w-3 h-3 flex justify-center items-center rounded-full bg-red-500"
-            onClick={handleCloseClick}
-          >
-            <BsX className="invisible group-hover:visible" />
-          </button>
-
-          <button
-            className="w-3 h-3 flex justify-center items-center rounded-full bg-amber-500"
-            onClick={handleMinimizeClick}
-          >
-            <CgMathMinus className="invisible group-hover:visible" />
-          </button>
-
-          <button
-            className="w-3 h-3 flex justify-center items-center rounded-full bg-green-500"
-            onClick={handleMaximizeClick}
-          >
-            <CgArrowsExpandLeft className="invisible group-hover:visible p-0.5" />
-          </button>
-        </div>
-        {title && (
-          <span className="font-semibold text-white text-sm">{title}</span>
-        )}
-      </div>
-
-      <div className="window-inner">{children}</div>
+      {children}
     </Rnd>
   );
 };
