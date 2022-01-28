8ab9c3cfa47001d950d04fc643e63037b6161866
Dave Quah <quahdave@gmail.com>
2022-01-28 23:56:57 +0800

    feat: update facetime app styling when inactive

--- src/components/AppFaceTime/AppFaceTime.tsx
+++ src/components/AppFaceTime/AppFaceTime.tsx
@@ -1,14 +1,20 @@
 import { FC, useEffect, useRef } from "react";
 import { IoIosLink } from "react-icons/io";
 import { IoVideocam } from "react-icons/io5";
+import classNames from "classnames";
 
 import { ApplicationKeys } from "reducers/applicationSlice";
+import { useAppSelector } from "hooks";
 import Button, { ButtonAppearance } from "base-components/Button";
 import Window, { DRAG_HANDLE_CLASS } from "components/Window";
 import WindowControls from "components/WindowControls";
 
 const AppFaceTime: FC<Record<string, never>> = () => {
+  const { activeTitle, facetime } = useAppSelector(
+    (state) => state.application
+  );
   const videoRef = useRef<HTMLVideoElement>(null);
+  const isAppActive = activeTitle === facetime.shortLabel;
 
   useEffect(() => {
     let stream: MediaStream | null = null;
@@ -52,7 +58,11 @@ const AppFaceTime: FC<Record<string, never>> = () => {
       <div className={`w-full h-full flex bg-neutral-900 ${DRAG_HANDLE_CLASS}`}>
         <div className="w-80 shrink-0 px-1.5 py-5">
           <div className="flex mb-5">
-            <WindowControls appKey={ApplicationKeys.FACETIME} isActive={true} />
+            <WindowControls
+              appKey={ApplicationKeys.FACETIME}
+              isActive={isAppActive}
+              inactiveColor="bg-neutral-700"
+            />
           </div>
 
           <div className="flex ml-3 mr-4">
@@ -65,7 +75,12 @@ const AppFaceTime: FC<Record<string, never>> = () => {
             </Button>
             <Button
               appearance={ButtonAppearance.DEFAULT}
-              className="w-full flex justify-center items-center bg-green-600 rounded-md text-sm tracking-tight text-gray-50 py-1"
+              className={classNames({
+                "w-full flex justify-center items-center rounded-md text-sm tracking-tight text-gray-50 py-1":
+                  true,
+                "bg-green-600": isAppActive,
+                "bg-neutral-600": !isAppActive,
+              })}
             >
               <IoVideocam className="w-3.5 h-3.5 mr-0.5" />
               New FaceTime
@@ -73,13 +88,13 @@ const AppFaceTime: FC<Record<string, never>> = () => {
           </div>
         </div>
 
-        <div className="grow bg-black">
+        <div className="grow bg-gray-900">
           <video
             ref={videoRef}
             autoPlay={true}
             className="w-full h-full object-cover"
             style={{ transform: "rotateY(180deg)" }}
-          ></video>
+          />
         </div>
       </div>
     </Window>
